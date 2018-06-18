906c5f2c61460b1791332047f4941e8f128d5896
Tharun <tharundintakurthi@gmail.com>
2018-06-18 15:49:58 +0530

    Add project files.

---
+++ OneTimeProgress/Controllers/StaffController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OneTimeProgress.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string userName, string password)
+        {
+            if (userName == "abc" && password == "abc")
+            {
+                return RedirectToAction("FlightsPage");
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult FlightsPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FlightsPage(string flightNumber)
+        {
+            return RedirectToAction("TaskDetails");
+        }
+        public ActionResult TaskDetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ OneTimeProgress/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OneTimeProgress.MvcApplication" Language="C#" %>
