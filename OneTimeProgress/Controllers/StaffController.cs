edd57fa1a8552da044503e7d8839e0267d34881f
Tharun <tharundintakurthi@gmail.com>
2018-06-18 21:15:34 +0530

    some ui changes

--- OneTimeProgress/Controllers/StaffController.cs
+++ OneTimeProgress/Controllers/StaffController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ExcelDataReader;
 using OneTimeProgress.BussinessEntity;
 using OneTimeProgress.BussinessLayer;
 
@@ -49,7 +52,10 @@ namespace OneTimeProgress.Controllers
         {
             string flightNumber = Session["flightNumber"].ToString();
             FlightDetails flightDetails = bussines.GetDetailsForOneFlight(flightNumber);
-            ViewBag.FlightDetails = flightDetails;
+            ViewBag.FlightNumber = flightDetails.FlightNumber;
+            ViewBag.Bay = flightDetails.Bay;
+            ViewBag.CurrentStation = flightDetails.CurrentStation;
+            ViewBag.Time = DateTime.Now.Hour +":"+ DateTime.Now.Minute;
             List<TaskLists> taskLists = bussines.GetTasksForParticularFlight(flightNumber);
             ViewBag.TaskLists = taskLists;
             return View();
@@ -61,6 +67,27 @@ namespace OneTimeProgress.Controllers
         }
         public string InsertTasks()
         {
+            string _path = @"C:\Users\hpadmin\Desktop\Standard\TaskDetails.xlsx";
+            FileStream stream = new FileStream(_path, FileMode.Open, FileAccess.Read);
+            SqlConnection con = new SqlConnection("Server=HIB30BWAX2; Initial Catalog = OneTimeProgress; User ID = sa; Password = Passw0rd@12;");
+            var reader = ExcelReaderFactory.CreateReader(stream);
+            if (true)
+            {
+                int j;
+                reader.Read();
+                con.Open();
+                while (reader.Read())
+                {
+                    SqlCommand cmd = new SqlCommand("InsertIntoTaskList @flightNumber,@taskDetail,@duration,@startTime", con);
+                    cmd.Parameters.AddWithValue("@flightNumber", reader.GetDouble(0));
+                    cmd.Parameters.AddWithValue("@taskDetail", reader.GetString(1));
+                    cmd.Parameters.AddWithValue("@startTime", reader.GetDouble(2));
+                    cmd.Parameters.AddWithValue("@duration", reader.GetDouble(3));
+                    
+                    j = cmd.ExecuteNonQuery();
+                }
+                con.Close();
+            }
             return "Inserted into Task List Table";
         }
     }
